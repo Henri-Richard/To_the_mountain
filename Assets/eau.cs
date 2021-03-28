c7f936214f9eac312eb475707624649a6adb8765
Henri-Richard <henrilessard09@gmail.com>
2021-03-28 19:43:22 -0400

    test

---
+++ Assets/eau.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class eau : MonoBehaviour
+{
+    private bool isSwiming;
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.tag == "player")
+        {
+            isSwiming = true;
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if(col.tag == "player")
+        {
+            isSwiming = false;
+        }
+    }
+
+    void Update()
+    {
+        if(isSwiming)
+        {
+            swim();
+        }
+    }
+
+    void swim()
+    {
+        
+    }
+}
